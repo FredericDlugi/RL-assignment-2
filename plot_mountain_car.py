d6fe7a3f02e79cd564e3368324d9034600dd275d
Frederic Dlugi <frederic.dlugi@student.uni-luebeck.de>
2021-05-24 09:57:56 +0200

    better plotting for MountainCar

--- plot_mountain_car.py
+++ plot_mountain_car.py
@@ -4,31 +4,64 @@ import numpy as np
 import matplotlib.pyplot as plt
 import gym
 
+def save_visits(folder):
+    visit_counts = np.load(os.path.join("mountain_car_results", folder, "visits.npy"))
+    visit_counts_states = visit_counts[:,:,0] + visit_counts[:,:,1] + visit_counts[:,:,2]
+
+    vel_ticks = [f"{i:.2f}" for i in np.linspace(env.observation_space.low[1], env.observation_space.high[1], 6)]
+    pos_ticks = [f"{i:.2f}" for i in np.linspace(env.observation_space.low[0], env.observation_space.high[0], 7)]
+    plt.yticks(np.linspace(0, visit_counts.shape[1]-1, 6), vel_ticks)
+    plt.xticks(np.linspace(0, visit_counts.shape[0]-1, 7), pos_ticks)
+    plt.ylabel("Velocity (m/s)")
+    plt.xlabel("Position (m)")
+    plt.imshow(np.rot90(visit_counts_states.astype(float)))
+
+    plt.savefig(os.path.join("plots", f"{folder}_visits.pdf"))
+    plt.clf()
+
+def save_policy(folder):
+    learned_policy = np.load(os.path.join("mountain_car_results", folder, "learned_policy.npy"))
+
+    indicies_1 = np.argwhere(learned_policy == 1)
+    plt.scatter(indicies_1[:,0], indicies_1[:,1], marker=".", label="no push")
+    indicies_0 = np.argwhere(learned_policy == 0)
+    plt.scatter(indicies_0[:,0], indicies_0[:,1], marker="<", label="push left")
+    indicies_2 = np.argwhere(learned_policy == 2)
+    plt.scatter(indicies_2[:,0], indicies_2[:,1], marker=">", label="push right")
+
+    vel_ticks = [f"{i:.2f}" for i in np.linspace(env.observation_space.low[1], env.observation_space.high[1], 6)]
+    pos_ticks = [f"{i:.2f}" for i in np.linspace(env.observation_space.low[0], env.observation_space.high[0], 7)]
+    plt.yticks(np.linspace(0, learned_policy.shape[1]-1, 6), vel_ticks)
+    plt.xticks(np.linspace(0, learned_policy.shape[0]-1, 7), pos_ticks)
+    plt.ylabel("Velocity (m/s)")
+    plt.xlabel("Position (m)")
+    plt.legend(loc='lower right',bbox_to_anchor=(1, 0))
+
+    plt.savefig(os.path.join("plots", f"{folder}_learned_policy.pdf"))
+    plt.clf()
+
 if __name__ == "__main__":
     env = gym.make('MountainCar-v0').env
     env.reset()
 
-    learned_policy = np.load(os.path.join("sarsa", "learned_policy.npy"))
-    visit_counts = np.load(os.path.join("sarsa", "visits.npy"))
-    episodic_returns = np.load(os.path.join("sarsa", "episodic_returns.npy"))
+    folder = "sarsa"
+    save_policy(folder)
+    sarsa_episodic_returns = np.load(os.path.join("mountain_car_results", folder, "episodic_returns.npy"))
+    save_visits(folder)
 
-    plt.suptitle("SARSA MountainCar-v0 results")
-    plt.subplot(1,2,1)
-    visit_counts_states = visit_counts[:,:,0] + visit_counts[:,:,1] + visit_counts[:,:,2]
-    plt.title("Visits of each state")
-    plt.xlabel("Velocity (m/s)")
-    plt.ylabel("Position (m)")
-    vel_ticks = [f"{i:.2f}" for i in np.linspace(env.observation_space.low[1], env.observation_space.high[1], 6)]
-    pos_ticks = [f"{i:.2f}" for i in np.linspace(env.observation_space.low[0], env.observation_space.high[0], 7)]
-    plt.xticks(np.linspace(0, visit_counts.shape[1]-1, 6), vel_ticks)
-    plt.yticks(np.linspace(0, visit_counts.shape[0]-1, 7), pos_ticks)
-    plt.imshow(visit_counts_states.astype(float))
+    folder = "q_learning"
+    save_policy(folder)
+    qlearn_episodic_returns = np.load(os.path.join("mountain_car_results", folder, "episodic_returns.npy"))
+    save_visits(folder)
 
-    plt.subplot(1,2,2)
+    plt.clf()
     plt.title("Return over episodes")
     plt.xlabel("episodes")
     plt.ylabel("Cummulative Return")
-    plt.plot(episodic_returns)
+    generations = np.arange(sarsa_episodic_returns.shape[0])
+    plt.scatter(generations, sarsa_episodic_returns, label="SARSA")
+    plt.scatter(generations, qlearn_episodic_returns, label="Q-Learning")
+    plt.legend()
 
-    plt.show()
+    plt.savefig(os.path.join("plots", "return_over_generations.pdf"))
 
